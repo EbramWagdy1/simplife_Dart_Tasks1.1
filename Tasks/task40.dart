718ebe5f1f4e2a92363c37a5081a1bccc8b847e9
Ebram  Wagdy Samy Zaki <wagdyebram891@gmail.com>
2025-09-02 14:17:35 +0300

    Update project files

--- Tasks/task40.dart
+++ Tasks/task40.dart
@@ -1,3 +1,20 @@
+import 'dart:io';
+
 void main(){
+ List<num>m=[];
+ while(true){
+   print("Enter the number");
+   num n=num.parse(stdin.readLineSync()!);
+   m.add(n);
+   print("Fo you want add another number (yes/no)? :");
+   String s= stdin.readLineSync()!.toLowerCase();
+   if(s=='no'){
+     break;
+   }
+ }
+
+ double average=m.reduce((a,b)=>a+b)/m.length;
+ print("The average is : $average");
+
 
 }
---
+++ Tasks/task5.dart
@@ -0,0 +1,21 @@
+void main(){
+  var x=5;
+  print("x=$x");
+  x=x*4;
+  print("x= 5 * 4 = $x");
+  x=x*4+3;
+  print("x= 5 * 4 + 3 = $x");
+  x=x*4+3-1;
+  print("x= 5 * 4 + 3 - 1 = $x");
+  double res=x*4+3-1*3.2;
+  print("x= 5 * 4 + 3 - 1 * 3.2 = $res");
+
+
+
+
+
+
+
+
+
+}
